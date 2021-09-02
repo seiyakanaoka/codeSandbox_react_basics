b40971bb5fda02e695cb2257abc2f535d0aed4c5
seiyakanaoka <78468016+seiyakanaoka@users.noreply.github.com>
2021-09-02 10:10:53 +0900

    split component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
+  const contentLedStyle = {
+    color: "pink",
+    fontSize: "18px"
+  };
   return (
     <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか?</p>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <p style={contentLedStyle}>お元気ですか?</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
