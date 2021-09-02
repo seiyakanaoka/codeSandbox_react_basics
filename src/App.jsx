9363fc0039344571a787f235a15960fa0637721d
seiyakanaoka <78468016+seiyakanaoka@users.noreply.github.com>
2021-09-02 12:10:42 +0900

    about Rerendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        // この論理演算子がない場合、setFaceShowFlagでstateを変更、再レンダリング後に
+        // またこのIf文を通り、再レンダリングしようとして、エラーが発生する
+        // 論理演算子を使うことで、再レンダリング後、同じ結果が来た場合はその情報を表示するように制御している。
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>٩( ᐛ )و</p>}
     </>
   );
 };
